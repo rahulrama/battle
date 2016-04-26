70085d74037fbfb4f821163750c3b2162a455549
Lexi Tran <hazukitran@gmail.com>
2016-04-26 21:59:11 +0700

    Step 18: Incomplete)

--- app.rb
+++ app.rb
@@ -2,9 +2,16 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Hello Battle!'
+    erb :index
   end
 
-  # start the server if ruby file executed directly
+  post '/names' do
+    @player_1 = params[:player1]
+    @player_2 = params[:player2]
+
+    erb :play.erb
+  end
+
+  # # start the server if ruby file executed directly
   run! if app_file == $0
 end
