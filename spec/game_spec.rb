438ac036b982c5f4441464d2732529294c61877f
rahulrama <rahulrama@gmail.com>
2016-04-27 17:51:12 +0100

    Step 26: Skinny Controllers

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,17 +1,28 @@
 
 require 'game'
 
-describe Game do 
-	subject(:game) {described_class.new}
-	let(:player_1) {double :player}
-	let(:player_2) {double :player}
+describe Game do
+	subject(:game) { described_class.new(player_1, player_2) }
+	let(:player_1) { double :player }
+	let(:player_2) { double :player }
+
+	describe "#initialize" do
+		it 'returns the name of Player 1' do
+			expect(game.player_1).to eq player_1
+		end
+
+		it 'returns the name of Player 2' do
+			expect(game.player_2).to eq player_2
+		end
+	end
+
 	describe '#attack' do
     it 'attacks the opponent' do
-      expect(player_2).to receive(:reduce)
-      game.attack(player_2)
+      expect(game.player_2).to receive(:reduce)
+      game.attack(game.player_2)
     end
   end
 
-  
+
 
 end
